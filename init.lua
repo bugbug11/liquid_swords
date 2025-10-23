814ea8d9ab85a60acd70972f252d891676c9cb59
bugbug11 <buggyboogles@gmail.com>
2025-10-23 16:24:46 -0600

    Improve liquid sword registration process
    Refactor liquid sword registration logic to handle source textures and descriptions correctly.

--- init.lua
+++ init.lua
@@ -1,28 +1,48 @@
 local liquid_swords = {} -- Table for storing runtime mod data
 liquid_swords.liquid_nodes = {} -- Table for storing liquid nodes
-for name, _ in pairs(core.registered_nodes) do -- For every registered node,
-  if core.get_node_group(name, "liquid") then -- If the node is in the liquid group,
-	  table.insert(liquid_swords.liquid_nodes, name) -- Add it to the table of liquid nodes
-  end
+for itemname, _ in pairs(core.registered_nodes) do -- For every registered node,
+	if core.get_node_group(itemname, "liquid") and luanti.get_node_group(name, "flowing") == 0 then -- If the node is a liquid source
+	table.insert(liquid_swords.liquid_nodes, itemname) -- Add it to the table of liquid nodes
+	end
 end
 
 for itemname in liquid_swords.liquid_nodes do -- For each liquid node
-  local shortname = itemname:match("([^:]+)$") -- Get the itemname without the modname
-  local name_from_source = false -- Actual itemname being used for the sword
-  if shortname:find("_source") then -- If "_source" is found in the itemname
-    name_from_source = name:gsub("_source", "") -- Remove "_source"
-  else
-    name_from_source = name -- Otherwise keep the itemname
-  end
-  local description_from_source = core.registered_nodes[itemname].description -- Get the node description
-  local description = false -- Actual description for the sword
-  if description_from_source:find(" Source") then -- If "Source" is found in the itemname
-    description = description_from_source:gsub(" Source", "") .. " Sword" -- Remove "Source"
-  else 
-    description = description_from_source .. " Sword" -- Otherwise keep the description
-  end
-  core.register_tool("liquid_swords:sword_"..name_from_source, {
-      -- TODO: Add tool properties
-      -- NOTE: The texture should be like "liquid_swords_base.png^(liquid_swords_blade.png^SOURCE_TEXTURE)"
-  })
+	local shortname = itemname:match("([^:]+)$") -- Get the itemname without the modname
+	local name_from_source = nil -- Actual itemname being used for the sword
+	if shortname:find("_source") then -- If "_source" is found in the itemname
+		name_from_source = name:gsub("_source", "") -- Remove "_source"
+  	else
+    	name_from_source = name -- Otherwise keep the itemname
+	end
+	local description_from_source = core.registered_nodes[itemname].description -- Get the node description
+	local description = nil -- Actual description for the sword
+	if description_from_source:find(" Source") then -- If "Source" is found in the itemname
+    	description = description_from_source:gsub(" Source", "") .. " Sword" -- Remove "Source"
+  	else 
+    	description = description_from_source .. " Sword" -- Otherwise keep the description
+	end
+	local source_texture = nil
+	if type(def.tiles) == "table" and def.tiles[1] then
+            source_texture = def.tiles[1]
+    elseif type(def.tiles) == "string" then
+        source_texture = def.tiles
+    end
+	local sword_image = "liquid_swords_base.png^(liquid_swords_blade.png^(source_texture))"
+	core.register_tool("liquid_swords:sword_"..name_from_source, {
+		description = description,
+		inventory_image = sword_image,
+		wield_image = sword_image,
+		groups = {sword = 1, liquid_sword = 1},
+		max_uses = 120
+		sound = "default_tool_hit",
+		tool_capabilities = {
+			full_punch_interval = 0.5,
+			groupcaps = {
+				fleshy = {
+					damage = 4,
+					uses = 1,
+				}
+			}
+		}
+	})
 end
